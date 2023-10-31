53e7f0e92be0859df265fe98270cae3dbf80adc3
Ritesh Jain <Ritesh.Jain@varian.com>
2023-10-31 19:52:25 +0530

    my first committed

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string rawString = """here is what want to do, 'i am not sure thoug' but I can tell your one thing "that if you do something" it won't waste""";
+        Console.WriteLine($$"""rawsting is: {{{rawString}}}""");
+        Console.WriteLine("Hello, World!123");
+        try
+        {
+            //fullname
+            FullName(null, "last");
+            dummy();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"in catch exception: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("in finally exception");
+        }
+
+
+
+    }
+
+    private static void dummy()
+    {
+        string? abc = "asda";
+        if (abc == null)
+        {
+            throw new Exception("wastage of time");
+        }
+        Console.WriteLine("after exception");
+    }
+
+    //c# 11.0 feature of null
+    //public static void FullName(string firstName, string lastName)
+    //{
+    //    if (firstName is null)
+    //    {
+    //        throw new ArgumentNullException(nameof(firstName));
+    //    }
+    //    if (lastName is null)
+    //    {
+    //        throw new ArgumentNullException(nameof(lastName));
+    //    }
+    //    // Body of the method
+    //}
+    //public static void FullName(string firstName!!, string lastName!!)
+    //{
+    //    // Body of the method
+    //}
+}
